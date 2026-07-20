c64b67d876d615c5020ce5e4b5861c802d2fa273
HarnesHa12 <harnesha10@gmail.com>
2026-07-20 13:59:44 +0200

    feat: plymouth + sync mode

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,6 +1,40 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    loader.timeout = 0;
+
+    plymouth = {
+      enable = true;
+      theme = "hexagon_dots_alt";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "hexagon_dots_alt" ];
+        })
+      ];
+    };
+
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    initrd.availableKernelModules = [
+      "amdgpu"
+      "nvidia"
+      "nvidia_modeset"
+      "nvidia_drm"
+      "nvidia_uvm"
+    ];
+
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=0"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=0"
+      "vt.global_cursor_default=0"
+      "nvidia_drm.modeset=1"
+    ];
+  };
 }
--- modules/hardware/graphics.nix
+++ modules/hardware/graphics.nix
@@ -11,8 +11,7 @@
     modesetting.enable = true;
     
     prime = {
-      offload.enable = true;
-      offload.enableOffloadCmd = true;
+      sync.enable = true;
 
       amdgpuBusId = "PCI:5@0:0:0";
       nvidiaBusId = "PCI:1@0:0:0";
