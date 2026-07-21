e977a140c127d9ccf0a16ebfecdc10a8816d3c36
HarnesHa12 <harnesha10@gmail.com>
2026-07-21 02:43:54 +0200

    fix(pnpm): global packages

--- home/user.nix
+++ home/user.nix
@@ -8,6 +8,14 @@
   home.file."bg.png".source = ../images/bg.png;
   home.file."fastfetch.png".source = ../images/fastfetch.png;
 
+  home.sessionVariables = {
+    PNPM_HOME = "$HOME/.local/share/pnpm";
+  };
+
+  home.sessionPath = [
+    "$HOME/.local/share/pnpm/bin"
+  ];
+
   home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true;
