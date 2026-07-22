58efe66c2411dc8f72b780c3c4de49af4deebbc9
HarnesHa12 <harnesha10@gmail.com>
2026-07-22 14:35:38 +0200

    feat: obs + vlc

--- modules/programs/desktop.nix
+++ modules/programs/desktop.nix
@@ -1,6 +1,27 @@
 { inputs, pkgs, ... }:
 
 {
+  programs.firefox.enable = true;
+
+  programs.obs-studio = {
+    enable = true;
+
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+
   environment.systemPackages = with pkgs; [
     brave
     vicinae
@@ -10,6 +31,7 @@
     mission-center
     adwaita-icon-theme
     proton-vpn
+    vlc
     inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 }
