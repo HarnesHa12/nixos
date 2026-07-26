3aa8ed3973a7b5e7ab42e1b2b995e6b2112a18be
HarnesHa12 <harnesha10@gmail.com>
2026-07-26 18:51:49 +0200

    fix(sddm): kwin compositor

--- modules/desktop/sddm.nix
+++ modules/desktop/sddm.nix
@@ -4,11 +4,21 @@
   services.displayManager.sddm = {
     enable = true;
 
-    wayland.enable = true;
+    wayland = {
+      enable = true;
+      compositor = "kwin";
+    };
     extraPackages = with pkgs; [
       kdePackages.qtmultimedia
     ];
     theme = "sddm-astronaut-theme";
+
+    settings = {
+      Theme = {
+        CursorTheme = "Adwaita";
+        CursorSize = 24;
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
